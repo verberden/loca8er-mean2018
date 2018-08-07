b38db11a72306025a60015cc818a895a03309170
Sergei <cergey@yandex.ru>
2018-08-07 23:18:12 +0700

    Add view templates

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,11 @@
+module.exports.homelist = function(req, res) {
+    res.render('location-list', { title: 'Home'});
+}
+
+module.exports.locationInfo = function(req, res) {
+    res.render('location-info', { title: 'Location Info'});
+}
+
+module.exports.addReview = function(req, res) {
+    res.render('location-review-form', { title: 'Add Review'});
+}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* GET Location pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', 
+           ctrlLocations.addReview);
+
+/* GET Other pages. */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;

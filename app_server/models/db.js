cebe18b38520c13d42b160380617673ac4cd47c5
Sergei <cergey@yandex.ru>
2018-08-11 01:41:41 +0700

    Add mongo db

---
+++ app_server/models/db.js
@@ -0,0 +1,45 @@
+var mongoose = require( 'mongoose' );
+var dbURI = 'mongodb://localhost/Loc8er';
+
+if (process.env.NODE_ENV === 'production') {
+    console.log(process.env);
+    dbURI = process.env.MONGOLAB_URI;
+}
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnect through '  + msg );
+        callback();
+    });
+};
+
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function() {
+        process.exit(0);
+    });
+});
+
+require('./locations');
